127d3d794838da035e9c71753ae5ca2bd8a4d214
Daryn <illusionneodi@gmail.com>
2024-12-15 20:42:32 +0500

    Update math.py

--- leetcode-problems/top150/math.py
+++ leetcode-problems/top150/math.py
@@ -1,1 +1,32 @@
+# () [9] Palindrome Number
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        return str(x) == str(x)[::-1]
+
+# () [66] Plus One
+
+# () [172] Factorial Trailing Zeros
+
+# () [69] Sqrt(x)
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        l, r = 1, x
+        
+        while l <= r:
+            mid = (l + r) // 2
+            mids = mid * mid
+
+            if mids == x:
+                return mid
+            elif mids < x:
+                l = mid + 1
+            else:
+                r = mid - 1
+        return r
+
+"""
+another solution, i prefer this one
+"""
+
+
 
