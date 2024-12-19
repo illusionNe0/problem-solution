cd5e0c6e6991b0fc9d1d12acb259a01d22533a25
Daryn <illusionneodi@gmail.com>
2024-12-19 22:12:59 +0500

    Update binary_search.py

--- leetcode-problems/top150/binary_search.py
+++ leetcode-problems/top150/binary_search.py
@@ -11,5 +11,26 @@ class Solution:
             else:
                 l = m + 1
         return l
-        
+
+
+# [74] Ssearch a 2D Matrix
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        # FUCKING O(LOG(M*N)) COMPLEXITY..
+
+        l, r = 0, len(matrix) - 1
+
+        # we'll use binary search for matrix[m] where target 'can be' in
+        while l <= r:
+            m = (l + r) // 2
+            if matrix[m][0] <= target <= matrix[m][-1]:
+                if matrix[m].count(target) != 0:
+                    return True
+                else:
+                    return False
+            elif matrix[m][0] > target:
+                r = m - 1
+            else:
+                l = m + 1
+        return False
 
