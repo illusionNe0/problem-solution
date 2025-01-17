a7f5a193ffed2b56263bc3e0e5fb35605b4a4ba3
Daryn <illusionneodi@gmail.com>
2025-01-17 16:53:36 +0500

    Update math.py

--- leetcode-problems/top150/math.py
+++ leetcode-problems/top150/math.py
@@ -8,6 +8,20 @@ class Solution:
 """
 [66] Plus One
 """
+class Solution:
+    def plusOne(self, digits: list) -> list:
+        if digits[-1] != 9:
+            digits[-1] += 1
+            return digits
+        else:
+            for i in range(len(digits) - 1, -1, -1):
+                if digits[i] == 9:
+                    digits[i] = 0
+                else:
+                    digits[i] += 1
+                    return digits
+            digits.insert(0, 1)
+            return digits
 
 
 """
